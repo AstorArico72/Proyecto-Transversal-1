3da149e3ae7b67b6ff7e7e97c868c96d712b09c7
Astor Aricó <82976048+AstorArico72@users.noreply.github.com>
2021-05-19 17:54:17 -0300

    Arreglado Alumno.java
    Borré el legajo porque éste es auto-incremental.

--- src/TPTransversal/Alumno.java
+++ src/TPTransversal/Alumno.java
@@ -5,33 +5,30 @@ import static universidad.Universidad.globalDTF;
 
 public class Alumno {
     private String nombre;
-    private int legajo;
+    //Borrado el legajo porque éste es auto-incremental.
     private int estado;
     private String correo;
     private String comentarios;
     private LocalDate fechaNacimiento;
     //Creo que ésos son todos los atributos privados.
     
-    public Alumno (int id, String nombre, int estado, String nacimiento) {
+    public Alumno (String nombre, int estado, String nacimiento) {
         this.nombre = nombre;
-        this.legajo = id; //Si el legajo es auto-incremental, entonces hay que borrar ése int.
         this.estado = estado;
         this.fechaNacimiento = LocalDate.parse (nacimiento, globalDTF); //El DTF estático de la clase Universidad.
     }
     /*
     SOBRECARGA DE CONSTRUCTORES, INICIO!
     */
-    public Alumno (int id, String nombre, int estado, String nacimiento, String correo) {
+    public Alumno (String nombre, int estado, String nacimiento, String correo) {
         this.nombre = nombre;
-        this.legajo = id;
         this.estado = estado;
         this.fechaNacimiento = LocalDate.parse (nacimiento, globalDTF);
         this.correo = correo;
     }
     
-    public Alumno (int id, String nombre, int estado, String nacimiento, String correo, String comentarios) {
+    public Alumno (String nombre, int estado, String nacimiento, String correo, String comentarios) {
         this.nombre = nombre;
-        this.legajo = id;
         this.estado = estado;
         this.fechaNacimiento = LocalDate.parse (nacimiento, globalDTF);
         this.correo = correo;
@@ -43,10 +40,6 @@ public class Alumno {
     
     public String getNombre () {
         return this.nombre;
-    }
-    
-    public int getID () {
-        return this.legajo;
     }
     
     public int getEstado () {
